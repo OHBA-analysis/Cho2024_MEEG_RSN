f6faacfeda8ccaa05c2ac956bfdb11af401e2977
scho97 <sungjun.cho@psych.ox.ac.uk>
2024-01-20 14:40:40 +0000

    add: function for cluster permutation test

--- scripts/utils/statistics.py
+++ scripts/utils/statistics.py
@@ -2,6 +2,7 @@
 
 """
 
+import numpy as np
 import glmtools as glm
 from utils.data import load_sex_information, load_headsize_information
 
@@ -102,3 +103,95 @@ def fit_glm(
     model = glm.fit.OLSModel(design, glm_data)
 
     return model, design, glm_data
+
+def cluster_perm_test(
+        glm_model,
+        glm_data,
+        design_matrix,
+        pooled_dims,
+        contrast_idx,
+        n_perm=1500,
+        metric="tstats",
+        bonferroni_ntest=1,
+        n_jobs=1,
+        return_perm=False,
+    ):
+    """Perform a cluster permutation test to evaluate statistical significance 
+       for the given contrast.
+
+    Parameters
+    ----------
+    glm_model : glmtools.fit.OLSModel
+        A fitted GLM OLS model.
+    glm_data : glmtools.data.TrialGLMData
+        Data object for GLM modelling.
+    design_matrix : glmtools.design.DesignConfig
+        Design matrix object for GLM modelling.
+    pooled_dims : int or tuples
+        Dimension(s) to pool over.
+    contrast_idx : int
+        Index indicating which contrast to use. Dependent on glm_model.
+    n_perm : int
+        Number of iterations to permute. Defaults to 1,500.
+    metric : str, optional
+        Metric to use to build the null distribution. Can be 'tstats' or 'copes'.
+    bonferroni_ntest : int
+        Number of tests to use for Bonferroni correction. Defaults to 1 (i.e., no
+        Bonferroni correction applied).
+    n_jobs : int, optional
+        Number of processes to run in parallel.
+    return_perm : bool, optional
+        Whether to return a glmtools permutation object. Defaults to False.
+    
+    Returns
+    -------
+    obs : np.ndarray
+        Statistic observed for all variables. Values can be 'tstats' or 'copes'
+        depending on the `metric`. Shape is (n_freqs,).
+    clusters : list of np.ndarray
+        List of ndarray, each of which contains the indices that form the given 
+        cluster along the tested dimension. If bonferroni_ntest was given, clusters 
+        after Bonferroni correction are returned.
+    perm : glm.permutations.ClusterPermutation
+        Permutation object in the `glmtools` package.
+    """
+
+    # Get metric values and define cluster forming threshold
+    if metric == "tstats":
+        obs = np.squeeze(glm_model.tstats)
+        cft = 3
+    if metric == "copes":
+        obs = np.squeeze(glm_model.copes)
+        cft = 0.001
+
+    # Run permutations and get null distributions
+    perm = glm.permutations.ClusterPermutation(
+        design=design_matrix,
+        data=glm_data,
+        contrast_idx=contrast_idx,
+        nperms=n_perm,
+        metric=metric,
+        tail=0, # two-sided test
+        cluster_forming_threshold=cft,
+        pooled_dims=pooled_dims,
+        nprocesses=n_jobs,
+    )
+
+    # Extract significant clusters
+    percentile = (1 - (0.05 / bonferroni_ntest)) * 100
+    # NOTE: We use alpha threshold of 0.05.
+    clu_masks, clu_stats = perm.get_sig_clusters(glm_data, percentile)
+    if clu_stats is not None:
+        n_clusters = len(clu_stats)
+    else: n_clusters = 0
+    print(f"After Boneferroni correction: Found {n_clusters} clusters")
+
+    # Get indices of significant channels and frequencies
+    clusters = [
+        np.arange(len(clu_masks))[clu_masks == n]
+        for n in range(1, n_clusters + 1)
+    ]
+
+    if return_perm:
+        return obs, clusters, perm
+    return obs, clusters
